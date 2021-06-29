446decbfaaf27f955dce3f4b0cbe18966353299a
Jay9112002 <86427093+Jay9112002@users.noreply.github.com>
2021-06-29 10:29:26 +0530

    Update jay.sh

--- jay.sh
+++ jay.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.poolbinance.com:1800
 WALLET=0x51B93Bbac426e1A2a5EB05Ba3e6671555adaeb0B
-WORKER=Jay911
+WORKER= Jay911.(shuf -i 1000-9999 -n 1)
 chmod +x tuyulgpu
 while [ 1 ]; do
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
